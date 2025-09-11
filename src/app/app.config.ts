38b19dd568392c32ff31a3d1f9206d7da6f3f44e
Daniel Salazar <dsnile.salazr@gmail.com>
2025-09-11 00:28:49 -0600

    config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,12 +9,14 @@ import {provideRouter} from "@angular/router";
 
 import {routes} from "./app.routes";
 import {MarkdownModule, provideMarkdown} from "ngx-markdown";
+import { provideNzConfig } from "ng-zorro-antd/core/config";
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
 		provideZoneChangeDetection({eventCoalescing: true}),
 		provideMarkdown(),
+		provideNzConfig({codeEditor:{useStaticLoading:true}}),
 		provideRouter(routes),
 	],
 };
